5ff097adc796f2e03d3355f8b639e7f6cee12443
Edo Freriks <edofre@gmail.com>
2017-03-02 23:13:29 +0100

    Dutch translations for default lang files

--- resources/lang/nl/auth.php
+++ resources/lang/nl/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed'   => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed'   => 'Deze gegevens komen niet overeen met onze data.',
+    'throttle' => 'Te veel inlog pogingen. Probeer het na :seconds seconden weer.',
 
 ];
