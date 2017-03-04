7aeb3a50bfe8312f32bbdf2122410e5bb7530ef0
Edo Freriks <edofre@gmail.com>
2017-03-04 20:48:59 +0100

    Add breadcrumbs view

---
+++ resources/views/layouts/_breadcrumbs.blade.php
@@ -0,0 +1,19 @@
+@if(!empty($breadcrumbs))
+    <ol class="breadcrumb">
+        @php
+            $keys = array_keys($breadcrumbs);
+            $last_key = array_pop($keys);
+        @endphp
+        @foreach($breadcrumbs as $key => $breadcrumb)
+            @if($last_key == $key)
+                <li class="active">
+                    {{ $breadcrumb['name'] }}
+                </li>
+            @else
+                <li>
+                    <a href="{{ $breadcrumb['route'] }}">{{ $breadcrumb['name'] }}</a>
+                </li>
+            @endif
+        @endforeach
+    </ol>
+@endif
