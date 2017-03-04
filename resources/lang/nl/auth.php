99cf9efdd976d784f582fd2e145115f8b890bca7
Edo Freriks <edofre@gmail.com>
2017-03-04 19:53:39 +0100

    Add nl translations

--- resources/lang/nl/auth.php
+++ resources/lang/nl/auth.php
@@ -13,6 +13,48 @@ return [
     |
     */
 
+    // auth.register
+    'register' => [
+        'register'         => 'Registreer',
+        'name'             => 'Naam',
+        'email_address'    => 'E-Mail adres',
+        'password'         => 'Wachtwoord',
+        'confirm_password' => 'Bevestig wachtwoord',
+    ],
+
+    // auth.login
+    'login'    => [
+        'login'                => 'Login',
+        'email_address'        => 'E-Mail adres',
+        'password'             => 'Wachtwoord',
+        'remember_me'          => 'Herinner mij',
+        'forgot_your_password' => 'Wachtwoord vergeten?',
+    ],
+
+    // auth.passwords.email
+    'email'    => [
+        'reset_password'           => 'Reset wachtwoord',
+        'email_address'            => 'E-Mail adres',
+        'send_password_reset_link' => 'Stuur wachtwoord resetlink',
+    ],
+
+    // auth.passwords.reset
+    'reset'    => [
+        'reset_password'   => 'Reset wachtwoord',
+        'email_address'    => 'E-Mail adres',
+        'password'         => 'Wachtwoord',
+        'confirm_password' => 'Bevestig wachtwoord',
+    ],
+
+    // General
+    'general'  => [
+        'login'             => 'Login',
+        'register'          => 'Registreer',
+        'logout'            => 'Logout',
+        'you_are_logged_in' => 'U bent ingelogd!',
+    ],
+
+    // Error messages
     'failed'   => 'Deze gegevens komen niet overeen met onze data.',
     'throttle' => 'Te veel inlog pogingen. Probeer het na :seconds seconden weer.',
 
