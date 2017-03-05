b2ad653f23758100e43cec151470b5592f20cee9
Edo Freriks <edofre@gmail.com>
2017-03-05 22:46:46 +0100

    Crud lang files

---
+++ resources/lang/nl/crud.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | CRUD
+    |--------------------------------------------------------------------------
+    */
+    /** Index */
+    'view_all'        => 'Bekijk alle :model',
+    'no_models_found' => 'Geen :model gevonden',
+    'count_models'    => ':count gevonden',
+
+    /** Create */
+    'create_model'    => 'Nieuw :model',
+    'created_model'   => ':model is succesvol opgeslagen',
+
+    /** Show */
+    'show_name'       => 'Toon :name',
+    'edit_current'    => 'Bewerk huidig :model',
+
+    /** Update */
+    'edit_name'       => 'Bewerk :name',
+    'show_current'    => 'Toon huidig :model',
+    'updated_model'   => ':model is succesvol bijgewerkt',
+
+    /** Delete */
+    'are_you_sure'    => 'Weet je zeker dat je dit wilt verwijderen?',
+    'deleted_model'   => ':model is succesvol verwijderd',
+
+    /** Actions */
+    'actions'         => 'Acties',
+    'create'          => 'Nieuw',
+    'read'            => 'Lees',
+    'show'            => 'Toon',
+    'edit'            => 'Bewerk',
+    'update'          => 'Update',
+    'delete'          => 'Verwijder',
+
+    /** Default attributes */
+    'id'              => 'ID',
+    'name'            => 'Naam',
+    'created_at'      => 'Aangemaakt op',
+    'updated_at'      => 'Bijgewerkt op',
+    'deleted_at'      => 'Verwijdert op',
+    'created_by'      => 'Aangemaakt door',
+    'updated_by'      => 'Bijgewerkt door',
+    'deleted_by'      => 'Verwijdert door',
+];
