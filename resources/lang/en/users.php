e39de6e9677339b47201a4a9be31b9b4e558f583
Edo Freriks <edofre@gmail.com>
2017-03-05 22:36:43 +0100

    Lang files

---
+++ resources/lang/en/users.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users
+    |--------------------------------------------------------------------------
+    */
+
+    'users' => 'Users',
+    'user'  => 'User',
+
+    'id'                    => 'ID',
+    'name'                  => 'Name',
+    'email'                 => 'Email',
+    'password'              => 'Password',
+    'password_confirmation' => 'Password confirmation',
+];
---
+++ resources/lang/nl/users.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users
+    |--------------------------------------------------------------------------
+    */
+
+    'users' => 'Gebruikers',
+    'user'  => 'Gebruiker',
+
+    'id'                    => 'ID',
+    'name'                  => 'Naam',
+    'email'                 => 'E-mail',
+    'password'              => 'Wachtwoord',
+    'password_confirmation' => 'Wacthwoord bevestiging',
+];
