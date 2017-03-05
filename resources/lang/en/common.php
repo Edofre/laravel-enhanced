1f93dafd127571d11aef13b63f87655617d5a173
Edo Freriks <edofre@gmail.com>
2017-03-05 22:47:52 +0100

    Common trans strings

---
+++ resources/lang/en/common.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common
+    |--------------------------------------------------------------------------
+    */
+    /** Home */
+    'welcome'                 => 'Welcome',
+    'welcome_to'              => 'Welcome to :name',
+
+    /** Search */
+    'search'                  => 'Search',
+    'search...'               => 'Search...',
+    'search_keyword'          => 'Keyword',
+    'search_validation_error' => 'Keyword has a minimum of 3 characters',
+    'reset'                   => 'Reset',
+
+    /** Export */
+    'export'                  => 'Export',
+
+    /** Sort */
+    'select_sort'             => 'Select sort',
+    'asc'                     => 'Ascending',
+    'desc'                    => 'Descending',
+
+    /** Uploads */
+    'download_file'           => 'Download file',
+    'no_file'                 => 'No file uploaded',
+    'upload'                  => 'Upload',
+    'upload_file'             => 'Upload file',
+    'uploading_new_file'      => 'Uploading a new file will remove the old file completely',
+
+    /** Errors */
+    'no_access'               => 'No access',
+    'error'                   => 'Error:',
+    'not_found'               => 'Not found.',
+    'forbidden'               => 'Forbidden',
+    'be_right_back'           => 'Be right back.',
+
+    /** Navigation */
+    'toggle_navigation'       => 'Toggle Navigation',
+
+    /** Flash */
+    'close'                   => 'Close',
+
+    /** Booleans */
+    'no'                      => 'No',
+    'yes'                     => 'Yes',
+
+    /** Gender */
+    'male'                    => 'Male',
+    'female'                  => 'Female',
+
+    /** Forms */
+    'send'                    => 'Send',
+    'select_model'            => 'Select :model',
+    'back'                    => 'Back',
+    'cancel'                  => 'Cancel',
+    'save'                    => 'Save',
+
+    /** Misc */
+    'example'                 => 'Example',
+    'slug'                    => 'Slug',
+];
---
+++ resources/lang/nl/common.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Common
+    |--------------------------------------------------------------------------
+    */
+    /** Home */
+    'welcome'                 => 'Welkom',
+    'welcome_to'              => 'Welkom bij :name',
+
+    /** Search */
+    'search'                  => 'Zoeken',
+    'search...'               => 'Zoek...',
+    'search_keyword'          => 'Zoekterm',
+    'search_validation_error' => 'Zoekterm moet minimaal 3 tekens bevatten',
+    'reset'                   => 'Reset',
+
+    /** Export */
+    'export'                  => 'Exporteer',
+
+    /** Sort */
+    'select_sort'             => 'Sorteer',
+    'asc'                     => 'Aflopend',
+    'desc'                    => 'Oplopend',
+
+    /** Uploads */
+    'download_file'           => 'Download bestand',
+    'no_file'                 => 'Geen bestand geupload',
+    'upload'                  => 'Upload',
+    'upload_file'             => 'Upload bestand',
+    'uploading_new_file'      => 'Als u een nieuw bestand upload zal het oude bestand verwijderd worden.',
+
+    /** Errors */
+    'no_access'               => 'Geen toegang',
+    'error'                   => 'Error:',
+    'not_found'               => 'Niet gevonden.',
+    'forbidden'               => 'Verboden',
+    'be_right_back'           => 'Ben zo terug.',
+
+    /** Navigation */
+    'toggle_navigation'       => 'Toggle navigatie',
+
+    /** Flash */
+    'close'                   => 'Sluiten',
+
+    /** Booleans */
+    'no'                      => 'Nee',
+    'yes'                     => 'Ja',
+
+    /** Gender */
+    'male'                    => 'Man',
+    'female'                  => 'Vrouw',
+
+    /** Forms */
+    'send'                    => 'Verstuurd',
+    'select_model'            => 'Selecteer :model',
+    'back'                    => 'Terug',
+    'cancel'                  => 'Annuleer',
+    'save'                    => 'Opslaan',
+
+    /** Misc */
+    'example'                 => 'Voorbeeld',
+    'slug'                    => 'Slug',
+];
