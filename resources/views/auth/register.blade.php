2ee43e7399e67cbffa97b993d28277744e877989
Edo Freriks <edofre@gmail.com>
2017-03-09 21:52:12 +0100

    Formatting

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">{{ trans('auth.register.register') }}</div>
                     <div class="panel-body">
@@ -35,10 +35,8 @@
                                     @endif
                                 </div>
                             </div>
-
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <label for="password" class="col-md-4 control-label">{{ trans('auth.register.password') }}</label>
-
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control" name="password" required>
 
@@ -49,15 +47,12 @@
                                     @endif
                                 </div>
                             </div>
-
                             <div class="form-group">
                                 <label for="password-confirm" class="col-md-4 control-label">{{ trans('auth.register.confirm_password') }}</label>
-
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                                 </div>
                             </div>
-
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
