294aacd4b219582f97eeefc07309e689a885e835
Edo Freriks <edofre@gmail.com>
2017-03-13 23:22:48 +0100

    403 view

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('page_title')
-    404 - {{ trans('error.not_found') }}
+    403 - {{ trans('error.forbidden') }}
 @endsection
 
 @section('content')
@@ -9,9 +9,9 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <div class="panel-heading">404</div>
+                    <div class="panel-heading">403</div>
                     <div class="panel-body">
-                        <h1>{{ trans('error.not_found') }}</h1>
+                        <h1>{{ trans('error.forbidden') }}</h1>
                     </div>
                 </div>
             </div>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -10,7 +10,6 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">Home</div>
-
                     <div class="panel-body">
                         // Home
                     </div>
