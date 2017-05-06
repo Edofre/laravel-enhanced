a603c3a33905014c4ae4f6d28c1ae293fe4e9dc0
Edo Freriks <edofre@gmail.com>
2017-05-06 23:22:56 +0200

    Add svg content helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Flash a message to the user and include the level of the message
  * @param        $message
@@ -8,4 +9,18 @@ function flash($message, $level = 'info')
 {
     session()->flash('flash_message', $message);
     session()->flash('flash_message_level', $level);
+}
+
+/**
+ * Returns the contents of an SVG file, this way we can properly style it using CSS
+ * @param $icon_name
+ * @return string
+ */
+function get_svg($icon_name)
+{
+    // Check if file exists, otherwise return the close icon
+    if (!file_exists("assets/images/$icon_name.svg")) {
+        $icon_name = 'icon-close';
+    }
+    return file_get_contents("assets/images/$icon_name.svg");
 }
